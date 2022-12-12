9f38b01e0303b6ec7dca52695ef47b3b1d601db2
Alin <alin.burtoi@outlook.com>
2022-12-12 14:25:46 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4b1dbde5-f187-4bff-bf99-3101774b524b';
+export const MODAL_CLOSE_SEC = 2.5;
